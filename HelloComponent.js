3fae9e8371632ce4b019aff9c57bdd88f0d8fa9f
Sanket Kharat <140420720+sanketkharat28@users.noreply.github.com>
2023-12-03 23:47:34 +0530

    Add files via upload

---
+++ HelloComponent.js
@@ -0,0 +1,12 @@
+// HelloComponent.js
+import React from 'react';
+
+const HelloComponent = () => {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+    </div>
+  );
+};
+
+export default HelloComponent;
---
+++ app.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import HelloComponent from './HelloComponent';
+
+const App = () => {
+  return (
+    <div>
+      <HelloComponent />
+    </div>
+  );
+};
+
+export default App;
